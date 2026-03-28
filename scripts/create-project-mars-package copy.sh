48c54b47fb1673b8b6f325a1c280fc3904ff3409
frank <f.hartmann@gmail.com>
2026-03-28 08:17:37 +0100

    add base + env properties

--- scripts/create-project-mars-package copy.sh
+++
@@ -1,8 +0,0 @@
-# test create
-
-project-create create \
-	--template_dir /opt/project_create/templates/mars_package/content \
-	--properties_file /opt/project_create/properties/mars_package/mars_default.properties \
-	--project_name $@
-
-
